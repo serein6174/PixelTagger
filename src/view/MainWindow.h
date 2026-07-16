e691f6bda6ba1cd37196bf4e2d6696cac8c192c6
Ling Ni <2811883990@qq.com>
2026-07-16 15:21:48 +0800

    feat(view): add multi-label toolbar UI and label view data

--- src/view/MainWindow.h
+++ src/view/MainWindow.h
@@ -1,9 +1,13 @@
 #pragma once
 
 #include <QMainWindow>
+#include <QVector>
 
+#include "common/presentation/LabelViewData.h"
+#include "common/types/EntityIds.h"
 #include "view/canvas/ImageCanvas.h"
 
+class QAction;
 class QComboBox;
 
 class MainWindow final : public QMainWindow {
@@ -22,23 +26,36 @@ signals:
     void previousImageRequested();
     void nextImageRequested();
     void labelNameChangeRequested(const QString& name);
+    void addLabelRequested(const QString& name, const QColor& color);
+    void removeLabelRequested(LabelId labelId);
+    void renameLabelRequested(LabelId labelId, const QString& name);
+    void labelColorChangeRequested(LabelId labelId, const QColor& color);
+    void currentLabelChangeRequested(LabelId labelId);
 
 public slots:
     void showStatus(const QString& message);
     void showError(const QString& message);
     void setCurrentLabelName(const QString& name);
+    void setLabels(const QVector<LabelViewData>& labels);
 
 private slots:
     void openImage();
     void openFolder();
     void openProject();
     void saveProject();
+    void addLabel();
+    void removeCurrentLabel();
+    void changeCurrentLabelColor();
 
 private:
     void createMenus();
     void createToolBar();
     void connectView();
+    LabelId currentComboLabelId() const noexcept;
+    QColor currentComboLabelColor() const;
 
     ImageCanvas* canvas_ = nullptr;
     QComboBox* labelComboBox_ = nullptr;
+    QAction* removeLabelAction_ = nullptr;
+    QAction* changeLabelColorAction_ = nullptr;
 };
